54f2c572bd3b47a30aa94234188b78168ac42323
AleksanSkywalker <it.karabatov@gmail.com>
2021-02-16 22:10:22 +0300

    16-02-2021

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Header.module.css';
+
+
+const Header = () => {
+    return <header className={s.header}>
+        <img src='https://avatanplus.com/files/resources/mid/5eafc6086fe6a171de9d910a.png' />
+    </header>
+
+}
+
+export default Header;
